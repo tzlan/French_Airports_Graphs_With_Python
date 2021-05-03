e365e84020e574a4df39f8973b04cccee3ad2735
tzlan <talzana55@gmail.com>
2021-05-03 22:30:19 +0200

    Lecture exel v2

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import pandas
+import pandas as pd
 import numpy as np
 import networkx
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 import community as community_louvain
 
+# Show all lines
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
-data = pandas.read_excel("d9c5dbbe269cee68a13ccbe7e7eda7f3728b39d5f62cd6e9a7ad5272fd5678.xlsx", "data_Trajets")
-
-
+data = pd.read_csv("d9c5dbbe269cee68a13ccbe7e7eda7f3728b39d5f62cd6e9a7ad5272fd5678.csv", sep=";")
 print(data)
-data.head()
