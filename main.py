f43bb077b87ab9fa62a00c647b34580d8936f277
tzlan <talzana55@gmail.com>
2021-05-07 15:36:26 +0200

    Lecture V4

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 import numpy as np
-import networkx
+import networkx as nx
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 import community as community_louvain
@@ -15,8 +15,22 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-data = pd.read_csv("d9c5dbbe269cee68a13ccbe7e7eda7f3728b39d5f62cd6e9a7ad5272fd5678.csv", sep=";")
-#print(data)
+#Declaration des tableaux
 
-#print(data.info)
-data.head()
+DatAvion = pd.read_csv("empreintecarbonneavion.csv", sep=";")
+DatDistanceAvion = pd.read_csv("DistanceAvion.csv", sep=";")
+DatVehicule = pd.read_csv("EmpreinteCarbonneVehicule.csv", sep=";")
+DatDistanceVehicule = pd.read_csv("DistanceVehicule.csv", sep=";")
+
+
+#Affichage des données
+
+print(DatAvion)
+print(DatDistanceAvion)
+print(DatVehicule)
+print(DatDistanceVehicule)
+
+
+
+#Normalisation
+
