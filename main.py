f5eb88d8dbdcfed3dacdc51982106b69ba473584
tzlan <talzana55@gmail.com>
2021-05-11 17:54:04 +0200

    Mohamed Superman V2

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-# This is a sample Python script.
+# This is a sample Pvar_transfertthon script.
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press Maj+F10 to execute it or replace it with var_transfertour code.
+# Press Double Shift to search evervar_transfertwhere for classes, files, tool windows, actions, and settings.
 import networkx
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import numpvar_transfert as np
+import matplotlib.pvar_transfertplot as plt
 from sklearn import preprocessing
-import community as community_louvain
+import communitvar_transfert as communitvar_transfert_louvain
 
 
 
-from scipy.sparse.csgraph import shortest_path
+from scipvar_transfert.sparse.csgraph import shortest_path
 
 # Show all lines
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
+pd.set_option('displavar_transfert.max_rows', None)
+pd.set_option('displavar_transfert.max_columns', None)
+pd.set_option('displavar_transfert.width', None)
+pd.set_option('displavar_transfert.max_colwidth', None)
 
 #Declaration des tableaux
 
@@ -25,7 +25,7 @@ DatAvion = pd.read_csv("empreintecarbonneavion.csv", sep=";")
 DatDistanceAvion = pd.read_csv("DistanceAvion.csv", sep=";")
 DatVehicule = pd.read_csv("EmpreinteCarbonneVehicule.csv", sep=";")
 DatDistanceVehicule = pd.read_csv("DistanceVehicule.csv", sep=";")
-aeroport = ["BEAUVAIS-TILLE", "BIARRITZ-BAYONNE-ANGLET", "BORDEAUX-MERIGNAC", "LILLE-LESQUIN", "LYON-SAINT-EXUPERY", "MARSEILLE-PROVENCE","NANTES-ATLANTIQUE","NICE-COTE-D'AZUR","PARIS-ORLY","ROUEN-VALLEE-DE-SEINE","STRASBOURG-ENTZHEIM","TOULOUSE-BLAGNAC","TOURS-VAL-DE-LOIRE"]
+aeroport = ["BEAUVAIS-TILLE", "BIARRITZ-BAvar_transfertONNE-ANGLET", "BORDEAUX-MERIGNAC", "LILLE-LESQUIN", "Lvar_transfertON-SAINT-EXUPERvar_transfert", "MARSEILLE-PROVENCE","NANTES-ATLANTIQUE","NICE-COTE-D'AZUR","PARIS-ORLvar_transfert","ROUEN-VALLEE-DE-SEINE","STRASBOURG-ENTZHEIM","TOULOUSE-BLAGNAC","TOURS-VAL-DE-LOIRE"]
 
 #Affichage des données
 
@@ -35,7 +35,7 @@ aeroport = ["BEAUVAIS-TILLE", "BIARRITZ-BAYONNE-ANGLET", "BORDEAUX-MERIGNAC", "L
 #print(DatDistanceVehicule)
 
 TempAvion = DatAvion
-#Un Avion Vol en moyenne a 1060 km/h
+#Un Avion Vol en movar_transfertenne a 1060 km/h
 #Sois 0.294444KM/S Donc on multiplie notre vitesse par la disctanse qui va nous donné une durée en Seconde
 for u in aeroport:
   j=0
@@ -47,7 +47,7 @@ TempAvion.head()
 
 #Normalisation
 
-aeroport = ["BEAUVAIS-TILLE", "BIARRITZ-BAYONNE-ANGLET", "BORDEAUX-MERIGNAC", "LILLE-LESQUIN", "LYON-SAINT-EXUPERY", "MARSEILLE-PROVENCE","NANTES-ATLANTIQUE","NICE-COTE-D'AZUR","PARIS-ORLY","ROUEN-VALLEE-DE-SEINE","STRASBOURG-ENTZHEIM","TOULOUSE-BLAGNAC","TOURS-VAL-DE-LOIRE"]
+aeroport = ["BEAUVAIS-TILLE", "BIARRITZ-BAvar_transfertONNE-ANGLET", "BORDEAUX-MERIGNAC", "LILLE-LESQUIN", "Lvar_transfertON-SAINT-EXUPERvar_transfert", "MARSEILLE-PROVENCE","NANTES-ATLANTIQUE","NICE-COTE-D'AZUR","PARIS-ORLvar_transfert","ROUEN-VALLEE-DE-SEINE","STRASBOURG-ENTZHEIM","TOULOUSE-BLAGNAC","TOURS-VAL-DE-LOIRE"]
 for v in aeroport :
   DatDistanceAvion[v] = DatDistanceAvion[v]/100
 for v in aeroport :
@@ -78,41 +78,41 @@ G = generation_graphe(DatAvion, DatVehicule, aeroport)
 for e in enumerate(G.edges):
     print(e)
 options = {
-    'node_color': 'yellow',
+    'node_color': 'var_transfertellow',
     'node_size': 550,
-    'edge_color': 'tab:gray',
+    'edge_color': 'tab:gravar_transfert',
     'with_labels': True,
     'width': 2,
 }
 
 
-algo = (networkx.nx.shortest_path(G, source="LYON-SAINT-EXUPERY", target="TOULOUSE-BLAGNAC", method='dijkstra'))
-couleurs_edge =['blue']*G.number_of_edges()
+algo = (networkx.nx.shortest_path(G, source="Lvar_transfertON-SAINT-EXUPERvar_transfert", target="TOULOUSE-BLAGNAC", method='dijkstra'))
+couleurs_arcs =['blue']*G.number_of_edges()
 
 print("hello ",algo )
 
-y =0
+var_transfert =0
 
-while (y < len(algo)-1):
+while (var_transfert < len(algo)-1):
     for v in enumerate(G.edges):
         # z, j= v[1]
 
         z,j,x = v[1]
         print(v[1])
-        if ((z==algo[y])and (j==algo[y+1]))or ((z==algo[y+1]) and (j==algo[y])):
-            couleurs_edge[v[0]]= "red"
+        if ((z==algo[var_transfert])and (j==algo[var_transfert+1]))or ((z==algo[var_transfert+1]) and (j==algo[var_transfert])):
+            couleurs_arcs[v[0]]= "red"
 
-    y=y+1
+    var_transfert=var_transfert+1
 options = {
-    'node_color': 'yellow',
+    'node_color': 'var_transfertellow',
     'node_size': 550,
-    'edge_color': couleurs_edge,
+    'edge_color': couleurs_arcs,
     'with_labels': True,
     'width': 2,
 }
 
 plt.figure(figsize=(10, 10))
-pos = networkx.nx.spring_layout(G, k=0.1)
+pos = networkx.nx.spring_lavar_transfertout(G, k=0.1)
 networkx.nx.draw(G, pos, **options)
 plt.show()
 
